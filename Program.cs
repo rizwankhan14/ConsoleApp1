5cd6bb8c20f52631f97a9ab6912cef080d27d216
Rizwan k <113101753+rizwankhan14@users.noreply.github.com>
2022-09-22 16:00:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;  //name space - collection of classes -> collection of functions/ methods
+
+namespace Basic_concepts
+{
+    //entry point 
+    //static - invoke once
+    //Main
+
+
+
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("WELCOME TO C-SHARP");
+        }
+
+    }
+}
